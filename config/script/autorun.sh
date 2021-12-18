0d3f292617b0013e726fab1f90ce6b648827e009
Nil0375 <khc0013579@gmail.com>
2021-12-18 17:05:37 +0800

    upload config

---
+++ config/script/autorun.sh
@@ -0,0 +1,6 @@
+source ~/.xprofile
+export PATH="$PATH:/home/$USER/.local/bin"
+xrandr --output HDMI2 --auto
+xrandr --output HDMI1 --auto --left-of HDMI2
+feh --bg-scale ~/.config/feh/wallpaper1206.jpg
+xset s 1800
