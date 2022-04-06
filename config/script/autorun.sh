ea406da2f0fb4ad65dc145cf4e3a532abbb07eb1
Nil0375 <khc0013579@gmail.com>
2022-04-06 21:05:05 +0800

    updata script

--- config/script/autorun.sh
+++ config/script/autorun.sh
@@ -1,6 +1,12 @@
-source ~/.xprofile
+export XIM=fcitx
+export XIM_PROGRAM=fcitx
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+export QT_QPA_PLATFORMTHEME=qt5ct
 export PATH="$PATH:/home/$USER/.local/bin"
 xrandr --output HDMI2 --auto
-xrandr --output HDMI1 --auto --left-of HDMI2
+xrandr --output HDMI1 --auto --left-of HDMI2                                                                      
 feh --bg-scale ~/.config/feh/wallpaper1206.jpg
-xset s 1800
+xset s 3600 3600
+xset dpms 3600 3600 10800
